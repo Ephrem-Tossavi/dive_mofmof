c4a9cbbfd88ec4c6c410e2df5f88ca487139ca9e
ephrem tossavi <cetossavi@gmail.com>
2021-09-02 17:06:08 +0100

    Demande de location de biens immobiliers

---
+++ app/models/property.rb
@@ -0,0 +1,6 @@
+class Property < ApplicationRecord
+    has_many :stations, dependent: :destroy
+    accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
+
+    validates :name, :rent, :adress, :building, :remarks, presence: true
+end
